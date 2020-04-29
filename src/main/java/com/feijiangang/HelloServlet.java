8ea51059abdf9829a4a7c8c05768a960062404ec
FJG0519 <454250089@qq.com>
2020-04-29 16:35:07 +0800

    init

---
+++ src/main/java/com/feijiangang/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.feijiangang;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @ClassName HelloServlet
+ * @Description TODO
+ * @Date 2020-04-29 15:30
+ * @Author fjg
+ **/
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hot deployment");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        req.setAttribute("currentTime",sdf.format(new Date()));
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+    }
+}
---
+++ src/test/java/com/feijiangang/AppTest.java
@@ -0,0 +1,9 @@
+package com.feijiangang;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+}
