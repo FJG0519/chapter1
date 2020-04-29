6b13734e4b707cbe61996ae6a4cab6ca8a8d57a5
FJG0519 <454250089@qq.com>
2020-04-29 17:15:44 +0800

    test

--- src/main/java/com/feijiangang/HelloServlet.java
+++ src/main/java/com/feijiangang/HelloServlet.java
@@ -20,7 +20,6 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("hot deployment");
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         req.setAttribute("currentTime",sdf.format(new Date()));
         req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
